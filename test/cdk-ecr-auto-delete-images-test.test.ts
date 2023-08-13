a05cb45d79fdc76b8c568a1437aa590dbf1ed241
go-to-k <24818752+go-to-k@users.noreply.github.com>
2023-08-14 01:23:45 +0900

    Initial commit

---
+++ test/cdk-ecr-auto-delete-images-test.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkEcrAutoDeleteImagesTest from '../lib/cdk-ecr-auto-delete-images-test-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-ecr-auto-delete-images-test-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkEcrAutoDeleteImagesTest.CdkEcrAutoDeleteImagesTestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
